b9de0f984acec9ea009cfd4a27070098eb68ebc2
Andrei Andrade <andrei.andrade@outlook.com>
2019-01-10 22:30:34 +0000

    refactor: Add append
    Added function to append data into json data.

--- api/v1/class/database.php
+++ api/v1/class/database.php
@@ -2,14 +2,33 @@
 
 class DataBase
 {
-    private $arrData = array();
 
     public function getData()
     {
         $data = file_get_contents("data.json");
-        $this->arrData = json_decode($data,true);
+        $arrData = json_decode($data,true);
 
-        return $this->arrData;
+        return $arrData;
+    }
+
+    public function appendData($newData){
+        $arrData = $this->getData();
+        $newData = json_decode($newData,true);
+        $newData['id'] = $this->getLastId($arrData);
+        array_push($arrData,$newData);
+        $jsonData = json_encode($arrData);
+        file_put_contents('data.json',$jsonData);
+    }
+
+    private function getLastId(array $arrData){
+        $count = count($arrData);
+        if($count > 0){
+            $idx = $count - 1;
+            $lastId = $arrData[$idx]['id'] + 1;
+        }else{
+            $lastId = 1;
+        }
+        return $lastId;
     }
 }
 
