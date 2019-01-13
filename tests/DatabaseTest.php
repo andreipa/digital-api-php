400f0d3f6a06316e750792dcde647455a0a32e9a
Andrei Andrade <andrei.andrade@outlook.com>
2019-01-13 08:35:51 +0000

    test: Add dependecy
    Added test dependecy on last id and check if is integer.

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -4,15 +4,29 @@ use PHPUnit\Framework\TestCase;
 
 class DatabaseTest extends TestCase
 {
+
     public function testArrayIsNotEmpty()
     {
+
         $dataBase = new DataBase;
-        
+
         $arrData = $dataBase->getData();
 
-        $count = count($arrData);
+        $this->assertGreaterThanOrEqual(1,count($arrData));
 
-        $this->assertGreaterThanOrEqual(1,$count);
+        return $arrData;
+    }
+
+    /**
+     * @depends testArrayIsNotEmpty
+     */
+    public function testLastIdIsAnInteger(array $arrData)
+    {
+        $dataBase = new DataBase;
+
+        $id = $dataBase->getLastId($arrData);
+
+        $this->assertGreaterThanOrEqual(1,$id);
     }
 }
 
