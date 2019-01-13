6c8ca4aecfee18336d2d5a81264d23d04458e55a
Andrei Andrade <andrei.andrade@outlook.com>
2019-01-13 12:07:57 +0000

    test: Add validation on file
    Added test to validate if data.json exists.

--- tests/CustomerTest.php
+++ tests/CustomerTest.php
@@ -4,7 +4,12 @@ use PHPUnit\Framework\TestCase;
 
 class CustomerTest extends TestCase
 {
-    
+    public function testIsFileJsonWritableOnCreation()
+    {
+        $fileJson = "api/v1/data.json";
+        $this->assertFileIsWritable($fileJson);
+    }
+
 }
 
 
