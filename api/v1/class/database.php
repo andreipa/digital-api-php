16f9e75146a24c6be507a9f0b3db653cf822f79c
Andrei Andrade <andrei.andrade@outlook.com>
2019-01-13 08:03:03 +0000

    style: Add comments
    Add some comments on each function.

--- api/v1/class/database.php
+++ api/v1/class/database.php
@@ -1,8 +1,20 @@
 <?php
 
+/**
+ * Class to simulate a database where data is pulled from json file
+ * 
+ * @author Andrei Andrade <andrei@andreiandrade.com>
+ * @version 1.0.0
+ */
 class DataBase
 {
 
+    /**
+     * Return an array of elements from a json file.
+     *
+     * @version 1.0.0
+     * @return array
+     */
     public function getData()
     {
         $data = file_get_contents("api/v1/data.json");
@@ -11,6 +23,13 @@ class DataBase
         return $arrData;
     }
 
+    /**
+     * Simulate the autoincrement from a database
+     *
+     * @version 1.0.0
+     * @param array $arrData
+     * @return int
+     */
     public function getLastId(array $arrData){
         $count = count($arrData);
         if($count > 0){
