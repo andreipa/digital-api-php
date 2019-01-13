f511c8cca49700fd7a6d6177df86b92498187a14
Andrei Andrade <andrei.andrade@outlook.com>
2019-01-13 11:56:45 +0000

    test: Add validation file json
    Added validation on file exists.

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -5,13 +5,20 @@ use PHPUnit\Framework\TestCase;
 class DatabaseTest extends TestCase
 {
 
+    public function testIsJsonFileExist()
+    {
+        $fileJson = "api/v1/data.json";
+        $this->assertFileExists($fileJson);
+
+    }
+
+    /**
+     * @depends testIsJsonFileExist
+     */
     public function testArrayIsNotEmpty()
     {
-
         $dataBase = new DataBase;
-
         $arrData = $dataBase->getData();
-
         $this->assertGreaterThanOrEqual(1,count($arrData));
 
         return $arrData;
@@ -23,9 +30,7 @@ class DatabaseTest extends TestCase
     public function testLastIdIsAnInteger(array $arrData)
     {
         $dataBase = new DataBase;
-
         $id = $dataBase->getLastId($arrData);
-
         $this->assertGreaterThanOrEqual(1,$id);
     }
 }
